2510f6be1222f3c7a38b34514c2455ec5766c986
Rahmat Hidayat <rhidayat674@gmail.com>
2023-12-05 05:18:08 +0700

    initial commit

---
+++ pages/events/[id].tsx
@@ -0,0 +1,16 @@
+import EventContent from "@/components/events-detail/EventContent";
+import { filterDetail } from "@/providers/EventRepository";
+import { useRouter } from "next/router";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const filteredData = router.query.id as string;
+
+  const filteredDetail = filterDetail(filteredData)
+
+  console.log(filteredDetail);
+
+  return <EventContent items={filteredDetail} />;
+}
+
+export default EventDetailPage;
